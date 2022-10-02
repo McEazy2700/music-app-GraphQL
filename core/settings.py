7b474cd9cd14e0de667b70ec46f189264f53c59d
Ezekiel Victor <codeepoch@gmail.com>
2022-10-02 12:48:16 -0400

    added railway to allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'music-app-graphql-production.up.railway.app']
 
 
 # Application definition
