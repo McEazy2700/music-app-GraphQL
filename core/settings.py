214fe9cf6e82e488f561a44e88955fac1d45491a
Ezekiel Victor <codeepoch@gmail.com>
2022-10-02 13:38:59 -0400

    changed staticfiles setting

--- core/settings.py
+++ core/settings.py
@@ -34,13 +34,13 @@ ALLOWED_HOSTS = ['127.0.0.1:8000', 'music-app-graphql-production.up.railway.app'
 INSTALLED_APPS = [
     "jet.dashboard",
     "jet",
-    "cloudinary_storage",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "cloudinary_storage",
     "cloudinary",
     "eazy_user",
     "graphene_django",
@@ -133,10 +133,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
-
-STATIC_ROOT = "static"
-
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
  
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
